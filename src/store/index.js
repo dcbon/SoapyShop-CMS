66a6f1a8fdbd5bfe039c04e01d228e62b7bc8911
dcbon <dcbonitaa@gmail.com>
2020-08-23 12:15:10 +0700

    forgot to change base url

--- src/store/index.js
+++ src/store/index.js
@@ -5,7 +5,7 @@ import router from '../router'
 
 Vue.use(Vuex)
 
-const baseURL = 'http://localhost:3000'
+const baseURL = 'https://soapyshop.herokuapp.com'
 
 export default new Vuex.Store({
   state: {
